d0c2542197d54d49f977cadf7af66d0988654915
Mauricio <mchirino@aniracloud.com>
2024-09-17 00:41:28 -0400

    creado page/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "page/index"
   resources :documentary_films
   resources :series
   resources :films
@@ -13,5 +14,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+   root "page#index"
 end
