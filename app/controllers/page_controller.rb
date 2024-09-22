5f8e5564915dbb30cef8e60f00c1698d035e3879
Mauricio <mchirino@aniracloud.com>
2024-09-22 14:34:51 -0400

    funciona la portada

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,8 +1,30 @@
 class PageController < ApplicationController
+
+  before_action :set_film, only: %i[   ]
+
+
+
+  # GET /films or /films.json
   def index
-    
+    @films = Film.limit(4) # Solo devuelve 4 películas
   end
 
 
 
+ 
+
+
+
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_film
+      @film = Film.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def film_params
+      params.require(:film).permit(:name, :sinopsis, :director, :imagen)
+    end
 end
